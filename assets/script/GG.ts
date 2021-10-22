865f10d6a5cb24d530266de7aaf2e0d760c0be84
ahhh <new_q8@163.com>
2021-10-22 17:16:37 +0800

    fix: 提交公共模块

--- assets/script/GG.ts
+++ assets/script/GG.ts
@@ -5,8 +5,12 @@
  */
 
 import { PrPathData } from './data/PrPathData';
+import { PrEventManager } from './manager/PrEventManager';
+import { PrLoadResouceManager } from './manager/PrLoadResourceManager';
 import { PrLogUtil } from './manager/PrLogUtil';
 export class GG {
     static Log = PrLogUtil; //日志
     static Path = PrPathData; //路径
+    static MgrEvent = PrEventManager; //事件管理器
+    static MgrLoad = PrLoadResouceManager; //加载管理器
 }
---
+++ assets/script/data/PrEnumData.ts
@@ -0,0 +1,8 @@
+/*
+ * Copyright (C) 2021, Flickering Inc. All rights reserved.
+ * Author: wenqianqin (wenqianqin@flickering.ai)
+ *
+ * Description:
+ */
+
+export enum EnumEvent {}
